6331603ffe8c7b139d25e8523f3f1b0ec769dcc0
santosh10949 <102519461+santosh10949@users.noreply.github.com>
2024-07-16 13:19:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
 import json 
+import folium
+from streamlit_folium import folium_static
 
 st.set_page_config(page_title="Rupeek KPI View", page_icon=":bar_chart:", layout="wide")
 
@@ -21,11 +23,22 @@ city_list=['Bangalore','Pune','Chennai','Hyderabad','Delhi','Mumbai','Ahmedabad'
            'Tirupati','Vadodara','Vijayawada','Visakhapatnam','Warangal']
 employee_city =  st.selectbox('Select your city', city_list, index=None )
 
-if st.checkbox("Click here to locate my locattion"):
-    loc = get_geolocation()
-    latitude=loc.get('coords').get('latitude')
-    longitude=loc.get('coords').get('longitude')
+
+if st.button("Click here to locate my locattion"):
+    
+        loc = get_geolocation()
+        if loc is not None :
+            print(type(loc))
+            latitude=loc.get('coords').get('latitude')
+            longitude=loc.get('coords').get('longitude')
+
+            m = folium.Map(location=[latitude, longitude], zoom_start=15)
+            folium.Marker([latitude, longitude], tooltip='You are here').add_to(m)
+            folium_static(m)
 
 
-    st.map(latitude=latitude, longitude=longitude,)
+            # st.map(latitude=latitude, longitude=longitude,)
+        else :
+              st.error('Click on Allow this Time to Capture Location')
+
 
