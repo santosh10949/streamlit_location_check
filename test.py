0e1dca291abdc245ec40e4dc590892b312bb4347
santosh10949 <102519461+santosh10949@users.noreply.github.com>
2024-07-16 13:04:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,31 @@
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
-import json
+import json 
 
-st.write(f"User agent is _{streamlit_js_eval(js_expressions='window.navigator.userAgent', want_output = True, key = 'UA')}_")
+st.set_page_config(page_title="Rupeek KPI View", page_icon=":bar_chart:", layout="wide")
 
-st.write(f"Screen width is _{streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')}_")
 
-st.write(f"Browser language is _{streamlit_js_eval(js_expressions='window.navigator.language', want_output = True, key = 'LANG')}_")
+st.title('Employee Home Location Tracker')
 
-st.write(f"Page location is _{ streamlit_js_eval(js_expressions='window.location.origin', want_output = True, key = 'LOC')}_")
+# Employee ID Input
+employee_id = st.text_input('Enter your Employee ID', '')
 
-# Copying to clipboard only works with a HTTP connection
+employee_name = st.text_input('Enter your Name', '')
 
-copy_to_clipboard("Text to be copied!", "Copy something to clipboard (only on HTTPS)", "Successfully copied" , component_key = "CLPBRD")
+employee_local_address = st.text_input('Enter your current home location', '')
 
-# Share something using the sharing API
-create_share_link(dict({'title': 'streamlit-js-eval', 'url': 'https://github.com/aghasemi/streamlit_js_eval', 'text': "A description"}), "Share a URL (only on mobile devices)", 'Successfully shared', component_key = 'shdemo')
+employee_pincode = st.text_input('Enter your home pincode', '')
 
-if st.checkbox("Check my location"):
+city_list=['Bangalore','Pune','Chennai','Hyderabad','Delhi','Mumbai','Ahmedabad','Aurangabad','Bhopal','Chandigarh','Coimbatore','Faridabad','Ghaziabad','Guntur','Gurgaon','Hassan','Indore','Jaipur','Jodhpur','Kakinada','Karimnagar','Kolhapur','Kolkata','Lucknow','Ludhiana','Mysore','Nagpur','Nashik','Nizamabad','Noida','Pondicherry','Rajahmundry',
+           'Rajkot','Shivmoga','Surat',
+           'Tirupati','Vadodara','Vijayawada','Visakhapatnam','Warangal']
+employee_city =  st.selectbox('Select your city', city_list, index=None )
+
+if st.checkbox("Click here to locate my locattion"):
     loc = get_geolocation()
-    st.write(f"Your coordinates are {loc}")
-    
-if True:
-    x = streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH',  want_output = True,)                
-    st.write(f"Width is {x}")
-    
+    latitude=loc.get('coords').get('latitude')
+    longitude=loc.get('coords').get('longitude')
+
+
+    st.map(latitude=latitude, longitude=longitude,)
+
